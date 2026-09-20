34df7d03f3c39c404fc0d98c2b044fb3aa93461e
nathan-real <nadelmard@ensc.fr>
2026-09-20 18:53:34 +0200

    Try to fixe the resume downloading

--- src/data/social.js
+++ src/data/social.js
@@ -3,8 +3,8 @@ const social = {
   linkedin: 'https://www.linkedin.com/in/nathan-adelmard-86a380392/',
   googlePlay: 'https://play.google.com/store/apps/dev?id=5994314402410562637',
   email: 'mailto/nathan.adelmard@sfr.fr',
-  resumeFr: `${import.meta.env.BASE_URL}resume/Nathan_ADELMARD_CV_FR.pdf`,
-  resumeEn: `${import.meta.env.BASE_URL}resume/Nathan_ADELMARD_CV_EN.pdf`,
+  resumeFr: `${import.meta.env.BASE_URL}/resume/Nathan_ADELMARD_CV_FR.pdf`,
+  resumeEn: `${import.meta.env.BASE_URL}/resume/Nathan_ADELMARD_CV_EN.pdf`,
 }
 
 export default social
