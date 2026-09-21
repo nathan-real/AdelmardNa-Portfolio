4764eff0e9c2d5943b38f2def5eb167cf3f60c4a
nathan-real <nadelmard@ensc.fr>
2026-09-21 10:28:44 +0200

    Mailto fixed

--- src/data/social.js
+++ src/data/social.js
@@ -2,7 +2,7 @@ const social = {
   github: 'https://github.com/nathan-real',
   linkedin: 'https://www.linkedin.com/in/nathan-adelmard-86a380392/',
   googlePlay: 'https://play.google.com/store/apps/dev?id=5994314402410562637',
-  email: 'mailto/nathan.adelmard@sfr.fr',
+  email: 'mailto:nathan.adelmard@sfr.fr',
   resumeFr: `${import.meta.env.BASE_URL}/resume/Nathan_ADELMARD_CV_FR.pdf`,
   resumeEn: `${import.meta.env.BASE_URL}/resume/Nathan_ADELMARD_CV_EN.pdf`,
 }
